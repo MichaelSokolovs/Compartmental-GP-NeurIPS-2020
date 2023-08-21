1859fe5cdd42b223c6ee27836a29509e0f651771
Michael Sokolovs <michael.sokolovs@gmail.com>
2023-08-21 15:06:10 +0100

    Temp - updated UK conterfactuals file (fix)

--- UK-counterfactuals.py
+++ UK-counterfactuals.py
@@ -12,7 +12,7 @@ import pandas as pds
 import torch
 from pyro.ops.stats import quantile
 
-import data_loader
+import data_loader_res
 import pyro_model.helper
 import pyro_model.helper
 from pyro_model.counterfactual_helper import get_R0_sooner_lockdown, get_R0_later_lockdown, get_counterfactual
@@ -56,32 +56,45 @@ prefix = 'trained_models/'
 
 pad = 24
 
-data_dict = data_loader.get_data_pyro(countries, smart_start=False, pad=pad, legacy=False)
+data_dict = data_loader_res.get_data_pyro(countries, smart_start=False, pad=pad, legacy=False)
 data_dict = pyro_model.helper.smooth_daily(data_dict)
 
-days = 0
+days = 14
 train_len = data_dict['cum_death'].shape[0] - days
+#print("TRAIN LENGTH " + str(train_len))
 covariates_actual = pyro_model.helper.get_covariates_intervention(data_dict, train_len, notime=True)
-Y_train = pyro_model.helper.get_Y(data_dict, train_len)
+#print("Y covariates_actual " + str(covariates_actual))
+# Y_train = pyro_model.helper.get_Y(data_dict, train_len)
+# print("Y TRAIN " + str(Y_train))
+#print("data_dict['cum_death'] " + str(data_dict['cum_death']))
+#print("data_dict['cum_death'][:train_len] " + str(data_dict['cum_death'][:train_len]))
 
-seed = 1
+seed = 3
 model_id = 'day-{}-rng-{}'.format(days, seed)
 
-with open(prefix + 'Loop{}/{}-predictive.pkl'.format(days, model_id), 'rb') as f:
+with open(prefix + 'Loop{}_custom_index_noH6/{}-predictive.pkl'.format(days, model_id), 'rb') as f:
     res = pickle.load(f)
 
-with open(prefix + 'Loop{}/{}-forecaster.csv'.format(days, model_id), 'rb') as f:
+with open(prefix + 'Loop{}_custom_index_noH6/{}-forecaster.csv'.format(days, model_id), 'rb') as f:
     forecaster = pickle.load(f)
 
 prediction = quantile(res['prediction'].squeeze(), (0.5,), dim=0).squeeze()
 
+start_ind = 14
 c = 0
-
+#print("data_dict['daily_death'] " + str(data_dict['daily_death']))
 dt_list = data_dict['date_list']
-start_date = data_dict['t_init'][c] + pad
+#print("DT LIST " + str(dt_list))
 
-plt.plot(dt_list[:-days - 1], np.diff(prediction[:, c]), label='Fitted SEIR')
-plt.plot(dt_list, data_dict['daily_death'][:, c], '.', label='acutal')
+start_date = data_dict['t_init'][c] + pad
+#print("data_dict['t_init'][c] " + str(data_dict['t_init'][c]))
+#print("dt_list[81] " + str(dt_list[81]))
+#print("data_dict['daily_death'][81,c] " + str(data_dict['daily_death'][81+2*pad,c]))
+#print("data_dict['daily_death'][:, c] " + str(data_dict['daily_death'][:, c].shape))
+
+plt.plot(dt_list[:-start_ind - 1], np.diff(prediction[:, c]), label='Fitted SEIR')
+#plt.plot(dt_list[:-days - 1], np.diff(prediction[:, c]), label='Fitted SEIR')
+plt.plot(dt_list[:-start_ind - 1], data_dict['daily_death'][:-start_ind - 1, c], '.', label='acutal')
 
 plt.gcf().autofmt_xdate()
 plt.title(countries[c])
@@ -96,11 +109,12 @@ R0_counter7a = get_R0_later_lockdown(R0, 7)
 R0_counter14a = get_R0_later_lockdown(R0, 14)
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
+#print("dt list shape " + str(dt_list[:start_ind].shape))
+#print("R0 " + str(quantile(R0, 0.025, dim=0)[0, :].shape))
+plt.fill_between(dt_list[:-start_ind], quantile(R0, 0.025, dim=0)[0, :], quantile(R0, 0.975, dim=0)[0, :], color='orange',
+                 alpha=0.3)
 
-plt.fill_between(dt_list, quantile(R0, 0.025, dim=0)[0, :], quantile(R0, 0.975, dim=0)[0, :], color='orange',
-                 alpha='0.3')
-
-plt.plot(dt_list, torch.mean(R0, dim=0)[0, :])
+plt.plot(dt_list[:-start_ind], torch.mean(R0, dim=0)[0, :])
 plt.axvline(datetime(2020, 3, 21), linestyle='--', color="black")
 
 pred_counter7 = get_counterfactual(data_dict, forecaster, res, R0_counter7)
@@ -117,13 +131,17 @@ pred_counter7a_me = pred_counter7a[1]
 pred_true = get_counterfactual(data_dict, forecaster, res, R0)[1, ...]
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
-plt.fill_between(dt_list[41:], pred_counter7_lo[40:, 0], pred_counter7_up[40:, 0], color='blue', alpha='0.3')
+print("dt_list length")
+print(dt_list[:-start_ind-1].shape)
+print("prediction length")
+print(pred_counter7_lo.shape)
+plt.fill_between(dt_list[:-start_ind-1], pred_counter7_lo[:, 0], pred_counter7_up[:, 0], color='blue', alpha=0.3)
 
-plt.fill_between(dt_list[41:], pred_counter7a_lo[40:, 0], pred_counter7a_up[40:, 0], color='orange', alpha='0.3')
+plt.fill_between(dt_list[:-start_ind-1], pred_counter7a_lo[:, 0], pred_counter7a_up[:, 0], color='orange', alpha=0.3)
 
-plt.plot(dt_list[41:], pred_counter7_me[40:, 0], label='One week earlier', color='navy')
-plt.plot(dt_list[41:], pred_true[40:, 0], label='Actual plan', color='black')
-plt.plot(dt_list[41:], pred_counter7a_me[40:, 0], label='One week later', color='red')
+plt.plot(dt_list[:-start_ind-1], pred_counter7_me[:, 0], label='One week earlier', color='navy')
+plt.plot(dt_list[:-start_ind-1], pred_true[:, 0], label='Actual plan', color='black')
+plt.plot(dt_list[:-start_ind-1], pred_counter7a_me[:, 0], label='One week later', color='red')
 
 plt.plot(datetime(2020, 3, 24), 0, marker='x', markersize=5, color="black", label='Lockdown start dates',
          linestyle="None")
@@ -135,14 +153,14 @@ plt.legend()
 plt.savefig('tables/Fig-3-UK-counterfactual.png', dpi=300)
 
 df_counterfactual = pds.DataFrame({
-    'dt': dt_list[41:],
-    'actual_plan': pred_true[40:, 0],
-    'wk_early_mean': pred_counter7_me[40:, 0],
-    'wk_early_lower': pred_counter7_lo[40:, 0],
-    'wk_early_upper': pred_counter7_up[40:, 0],
-    'wk_later_mean': pred_counter7a_me[40:, 0],
-    'wk_later_lower': pred_counter7a_lo[40:, 0],
-    'wk_later_upper': pred_counter7a_up[40:, 0],
+    'dt': dt_list[:-start_ind-1],
+    'actual_plan': pred_true[:, 0],
+    'wk_early_mean': pred_counter7_me[:, 0],
+    'wk_early_lower': pred_counter7_lo[:, 0],
+    'wk_early_upper': pred_counter7_up[:, 0],
+    'wk_later_mean': pred_counter7a_me[:, 0],
+    'wk_later_lower': pred_counter7a_lo[:, 0],
+    'wk_later_upper': pred_counter7a_up[:, 0],
 
 })
 
